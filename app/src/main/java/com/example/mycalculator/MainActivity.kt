e2107b0110361f0825571d298d78c5af539062bb
mauroLonghi <mauro.longhi@globant.com>
2019-03-19 17:12:18 -0300

    inluce activity_mai.xml

--- app/src/main/java/com/example/mycalculator/MainActivity.kt
+++ app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -2,9 +2,6 @@ package com.example.mycalculator
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v7.widget.GridLayoutManager
-import android.support.v7.widget.RecyclerView
-import com.example.mycalculator.adapters.RecyclerAdapter
 
 class MainActivity : AppCompatActivity() {
 
--- app/src/main/java/com/example/mycalculator/mvp/presenter/CountPresenter.kt
+++
@@ -1,7 +0,0 @@
-package com.example.mycalculator.mvp.presenter
-
-public class CountPresenter{
-
-    fun CountPresenter() {
-    }
-}
