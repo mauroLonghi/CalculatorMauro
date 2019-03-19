8dbfc770c7e8f64f0bcebce9032d68b1d6382fe9
mauroLonghi <mauro.longhi@globant.com>
2019-03-19 14:54:42 -0300

    first commit, include mvp package

--- app/src/main/java/com/example/mycalculator/MainActivity.kt
+++ app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.mycalculator
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.RecyclerView
+import com.example.mycalculator.adapters.RecyclerAdapter
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
---
+++ app/src/main/java/com/example/mycalculator/mvp/presenter/CountPresenter.kt
@@ -0,0 +1,7 @@
+package com.example.mycalculator.mvp.presenter
+
+public class CountPresenter{
+
+    fun CountPresenter() {
+    }
+}
