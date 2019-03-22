e3e3b55ca5570369bff146bf8ba11950c37bc649
mauroLonghi <mauro.longhi@globant.com>
2019-03-22 18:40:58 -0300

    corrections done

--- app/src/main/java/com/example/mycalculator/MainActivity.kt
+++ app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -24,7 +24,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private var presenter: CountPresenter? = null
+    lateinit var presenter: CountPresenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,55 +33,55 @@ class MainActivity : AppCompatActivity() {
         presenter = CountPresenter(CountModel(), CalculatorView(this))
 
         button_0.setOnClickListener {
-            presenter?.valueNumber(TEXT_BUTTON_ZERO)
+            presenter.valueNumber(TEXT_BUTTON_ZERO)
         }
         button_1.setOnClickListener {
-            presenter?.valueNumber(TEXT_BUTTON_ONE)
+            presenter.valueNumber(TEXT_BUTTON_ONE)
         }
         button_2.setOnClickListener {
-            presenter?.valueNumber(TEXT_BUTTON_TWO)
+            presenter.valueNumber(TEXT_BUTTON_TWO)
         }
         button_3.setOnClickListener {
-            presenter?.valueNumber(TEXT_BUTTON_THREE)
+            presenter.valueNumber(TEXT_BUTTON_THREE)
         }
         button_4.setOnClickListener {
-            presenter?.valueNumber(TEXT_BUTTON_FOUR)
+            presenter.valueNumber(TEXT_BUTTON_FOUR)
         }
         button_5.setOnClickListener {
-            presenter?.valueNumber(TEXT_BUTTON_FIVE)
+            presenter.valueNumber(TEXT_BUTTON_FIVE)
         }
         button_6.setOnClickListener {
-            presenter?.valueNumber(TEXT_BUTTON_SIX)
+            presenter.valueNumber(TEXT_BUTTON_SIX)
         }
         button_7.setOnClickListener {
-            presenter?.valueNumber(TEXT_BUTTON_SEVEN)
+            presenter.valueNumber(TEXT_BUTTON_SEVEN)
         }
         button_8.setOnClickListener {
-            presenter?.valueNumber(TEXT_BUTTON_EIGHT)
+            presenter.valueNumber(TEXT_BUTTON_EIGHT)
         }
         button_9.setOnClickListener {
-            presenter?.valueNumber(TEXT_BUTTON_NINE)
+            presenter.valueNumber(TEXT_BUTTON_NINE)
         }
         button_divide.setOnClickListener {
-            presenter?.valueOperator(TEXT_BUTTON_DIV)
+            presenter.valueOperator(TEXT_BUTTON_DIV)
         }
         button_product.setOnClickListener {
-            presenter?.valueOperator(TEXT_BUTTON_MULT)
+            presenter.valueOperator(TEXT_BUTTON_MULT)
         }
         button_sum.setOnClickListener {
-            presenter?.valueOperator(TEXT_BUTTON_PLUS)
+            presenter.valueOperator(TEXT_BUTTON_PLUS)
         }
         button_difference.setOnClickListener {
-            presenter?.valueOperator(TEXT_BUTTON_MINUS)
+            presenter.valueOperator(TEXT_BUTTON_MINUS)
         }
         button_del.setOnClickListener {
-            presenter?.deleteLast()
+            presenter.deleteLast()
         }
         button_clear.setOnClickListener {
-            presenter?.clearSet()
+            presenter.clearSet()
         }
         button_result.setOnClickListener {
-            presenter?.valueResult()
+            presenter.valueResult()
         }
 
     }
